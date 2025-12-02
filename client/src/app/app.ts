e488e0a5e167b5589a28bf14320e0a0fb853f03f
Steven Salib <ssalib2@uwo.ca>
2025-12-01 23:15:29 -0500

    Added a Global Page with all components attaching to it.

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -3,6 +3,7 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.css'
---
+++ client/src/app/pages/budget/budget.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-budget',
+  standalone: true,
+  imports: [],
+  templateUrl: './budget.html',
+  styleUrl: './budget.css',
+})
+export class Budget {
+
+}
