faa77b600f9604bddb6a6a0bc51dad76fbcc9029
stevensalib05 <stevensalib2005@gmail.com>
2025-12-27 10:21:33 -0500

    Modified the nav bar slightly. Logout now sends you back to the welcome screen.

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,16 +1,18 @@
 import './Home.css';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+  const navigate = useNavigate();
 
   return (
     <>
       <div className='welcomecontainer'>
         <div className='navbar'>
           <ul className='navbarcontent'>
-            <li className='navbaritem'><a href="#Home">Home</a></li>
+            <li className='navbaritem'><a href="">Home</a></li>
             <li className='navbaritem'><a href="#Accounts">Accounts</a></li>
             <li className='navbaritem'><a href="#Budget">Budget</a></li>
-            <li className='navbaritem'><a href="#Logout">Logout</a></li>
+            <li className='navbaritem' id='logout'><a href="" onClick={() => navigate('/')}>Logout</a></li>
           </ul>
         </div>
         <div className='welcome'>
