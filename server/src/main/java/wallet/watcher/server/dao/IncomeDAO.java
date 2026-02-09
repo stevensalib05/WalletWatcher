1aaa5e07a002787e93852ad9e53c80523f4c3c48
stevensalib05 <stevensalib2005@gmail.com>
2026-02-09 09:37:04 -0500

    added more temp backend functions. fixed button placement (i swear sql is coming soon trust)

--- server/src/main/java/wallet/watcher/server/dao/IncomeDAO.java
+++ server/src/main/java/wallet/watcher/server/dao/IncomeDAO.java
@@ -1,4 +1,27 @@
 package wallet.watcher.server.dao;
 
+import wallet.watcher.server.entities.Income;
+import wallet.watcher.server.storage.Incomes;
+
 public class IncomeDAO {
+    private static Incomes incomes = new Incomes();
+
+    public Incomes getIncomes() {
+        return incomes;
+    }
+
+    public void addIncome(Income income) {
+        incomes.getIncomes().add(income);
+    }
+
+    public Incomes getIncomesByEmail(String email) {
+        Incomes results = new Incomes();
+        for (Income income : incomes.getIncomes()) {
+            if (income.getEmail().equals(email) && income.getEmail() != null) {
+                results.getIncomes().add(income);
+            }
+        }
+
+        return results;
+    }
 }
